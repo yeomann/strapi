7706a076017139811957110a181e99a28aa63c94
soupette <cyril.lpz@gmail.com>
2018-12-17 15:23:49 +0100

    Force restart app

--- test/start.js
+++ test/start.js
@@ -2,38 +2,45 @@ const spawn = require('child_process').spawn;
 const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
-const cypress = require('cypress')
+const cypress = require('cypress');
 const { deleteApp } = require('./helpers/deleteFolder');
 
 const strapiBin = path.resolve('./packages/strapi/bin/strapi.js');
+let hasAlreadyForcedServerRestart = false;
 const appName = 'testApp';
 let testExitCode = 0;
 let appStart;
 
 const databases = {
   mongo: `--dbclient=mongo --dbhost=127.0.0.1 --dbport=27017 --dbname=strapi-test-${new Date().getTime()} --dbusername= --dbpassword=`,
-  postgres: '--dbclient=postgres --dbhost=127.0.0.1 --dbport=5432 --dbname=strapi-test --dbusername= --dbpassword=',
-  mysql: '--dbclient=mysql --dbhost=127.0.0.1 --dbport=3306 --dbname=strapi-test --dbusername=root --dbpassword=root'
+  postgres:
+    '--dbclient=postgres --dbhost=127.0.0.1 --dbport=5432 --dbname=strapi-test --dbusername= --dbpassword=',
+  mysql:
+    '--dbclient=mysql --dbhost=127.0.0.1 --dbport=3306 --dbname=strapi-test --dbusername=root --dbpassword=root',
 };
 
-const {runCLI: jest} = require('jest-cli/build/cli');
+const { runCLI: jest } = require('jest-cli/build/cli');
 
 const main = async () => {
   const clean = () => {
-    return new Promise(async (resolve) => {
+    return new Promise(async resolve => {
       try {
         fs.accessSync(appName);
         await deleteApp(path.resolve(appName));
-      } catch(err) {
+      } catch (err) {
         // Silent
       }
       resolve();
     });
   };
 
-  const generate = (database) => {
+  const generate = database => {
     return new Promise((resolve, reject) => {
-      const appCreation = spawn('node', `${strapiBin} new ${appName} --dev ${database}`.split(' '), { detached: true });
+      const appCreation = spawn(
+        'node',
+        `${strapiBin} new ${appName} --dev ${database}`.split(' '),
+        { detached: true },
+      );
 
       appCreation.stdout.on('data', data => {
         console.log(data.toString().trim());
@@ -55,18 +62,27 @@ const main = async () => {
     return new Promise((resolve, reject) => {
       try {
         shell.cd('./testApp');
-        appStart = shell.exec(`strapi start`, { async: true, silent: true });
-        appStart.stdout.on('data', (data) => {
+        appStart = shell.exec('strapi start', { async: true, silent: true });
+        appStart.stdout.on('data', data => {
           if (data.includes('To shut down your server')) {
             shell.cd('..');
+
             return resolve();
+          } else if (
+            data.includes('POST /content-type-builder/model') &&
+            !hasAlreadyForcedServerRestart
+          ) {
+            console.log('ooooo');
+            process.kill(appStart.pid);
+            hasAlreadyForcedServerRestart = true;
+
+            return start();
           } else {
             console.log(data.trim());
           }
         });
-
       } catch (e) {
-        console.log(e)
+        console.log(e);
         if (typeof appStart !== 'undefined') {
           process.kill(appStart.pid);
         }
@@ -75,26 +91,30 @@ const main = async () => {
     });
   };
 
-
   const test = () => {
-    return new Promise(async (resolve) => {
+    return new Promise(async resolve => {
       // Run setup tests to generate the app.
-      await jest({
-        passWithNoTests: true,
-        testURL: 'http://localhost/'
-      }, [process.cwd()]);
+      await jest(
+        {
+          passWithNoTests: true,
+          testURL: 'http://localhost/',
+        },
+        [process.cwd()],
+      );
 
       const packagesPath = path.resolve(process.cwd(), 'packages');
 
-      const packages = fs.readdirSync(packagesPath)
-        .filter(file => file.indexOf('strapi') !== -1);
+      const packages = fs.readdirSync(packagesPath).filter(file => file.indexOf('strapi') !== -1);
 
       // Run tests in every packages.
       for (let i in packages) {
-        await jest({
-          passWithNoTests: true,
-          testURL: 'http://localhost/'
-        }, [path.resolve(packagesPath, packages[i])]);
+        await jest(
+          {
+            passWithNoTests: true,
+            testURL: 'http://localhost/',
+          },
+          [path.resolve(packagesPath, packages[i])],
+        );
       }
 
       resolve();
@@ -102,13 +122,16 @@ const main = async () => {
   };
 
   const cypressTest = () => {
-    const config = Object.assign({ spec: './packages/**/test/front/integration/*' }, process.env.npm_config_browser === 'true' ? { browser: 'chrome' } : {});
-    
-    return cypress
-      .run(config);
-  }
+    const config = Object.assign(
+      { spec: './packages/**/test/front/integration/*' },
+      // { spec: './packages/strapi-plugin-users-permissions/test/front/integration/*' },
+      process.env.npm_config_browser === 'true' ? { browser: 'chrome' } : {},
+    );
 
-  const testProcess = async (database) => {
+    return cypress.run(config);
+  };
+
+  const testProcess = async database => {
     try {
       await clean();
       await generate(database);
